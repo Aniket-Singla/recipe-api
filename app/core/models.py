6d6cfa778ad54cf9f7f28e03a9be05bba66269cb
Aniket-Singla <1singlaaniket@gmail.com>
2019-09-23 15:47:02 +0530

    Added list recipe feature

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 from django.conf import settings
-import uuid, os
+import uuid
+import os
 # Create your models here.
 
 
